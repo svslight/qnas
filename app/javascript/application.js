76195d28ef4609b1128cda62388e9672ac0b3b47
Svetlana Solodovnikova <svslight@yandex.ru>
2023-01-10 17:07:45 +0500

    7 Ajax - Settings

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import 'jquery'
+import "jquery_ujs"
+//import "popper"
+//import "bootstrap"
+
+import "./custom/answers"
+
+import "@popperjs/core";
+import "bootstrap";
+
+document.addEventListener("turbo:load", function () {
+  // This code is copied from Bootstrap's docs. See link below.
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
---
+++ app/javascript/custom/answers.js
@@ -0,0 +1,9 @@
+
+$(document).on('turbo:load', function(){
+  $('.question_answers').on('click', '.edit-answer-link', function(e) {
+      e.preventDefault();
+      $(this).hide();
+      var answerId = $(this).data('answerId');
+      $('form#edit-answer-' + answerId).removeClass('hidden');
+  })
+});
