e23ddb46fd87b1268a5e7f4723a8d6475f69f34f
Svetlana Solodovnikova <svslight@yandex.ru>
2023-01-15 21:10:26 +0500

    9 Nested Form: Generate Model Link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_12_070747) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_082837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_070747) do
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
+  create_table "links", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.bigint "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_links_on_question_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
@@ -79,5 +88,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_070747) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users", column: "author_id"
+  add_foreign_key "links", "questions"
   add_foreign_key "questions", "users", column: "author_id"
 end
