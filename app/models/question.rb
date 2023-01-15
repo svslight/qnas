bd8f8883240de2f5dd8ab3c4c58bcc5077b58bc8
Svetlana Solodovnikova <svslight@yandex.ru>
2023-01-15 21:17:09 +0500

    9 Nested form: Added Associations and Macros

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,14 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+  has_many :links, dependent: :destroy
+
   belongs_to :author, class_name: 'User'
 
-  # Декларация: модель имеет прикрепленный файл, :files - имя атрибута для обращения
-  # has_one_attached :file  
+  # Декларация: модель имеет прикрепленные файлы
   has_many_attached :files
 
-  validates :title, :body, presence: true
-  
+  # Makros принимает атрибуты для модели Links, при создании Вопроса создавает ссылки
+  accepts_nested_attributes_for :links, reject_if: :all_blank
+
+  validates :title, :body, presence: true  
 end
