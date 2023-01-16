39a2606e2e7223636f8bc393c90134f2bc46cd08
Svetlana Solodovnikova <svslight@yandex.ru>
2023-01-16 19:25:07 +0500

    9 Nested form: Added Polymorphic Association for Links

--- db/schema.rb
+++ db/schema.rb
@@ -57,10 +57,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_15_082837) do
   create_table "links", force: :cascade do |t|
     t.string "name"
     t.string "url"
-    t.bigint "question_id"
+    t.string "linkable_type"
+    t.bigint "linkable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_links_on_question_id"
+    t.index ["linkable_type", "linkable_id"], name: "index_links_on_linkable"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -88,6 +89,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_15_082837) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users", column: "author_id"
-  add_foreign_key "links", "questions"
   add_foreign_key "questions", "users", column: "author_id"
 end
