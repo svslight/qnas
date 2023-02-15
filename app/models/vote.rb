0baa73459b8aa2721dae062d1d09659734a6119d
Svetlana Solodovnikova <svslight@yandex.ru>
2023-02-15 15:37:34 +0500

    11 User can add comments to question

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,20 +5,19 @@ class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
 
   validates :state, presence: true, inclusion: { in: [-1, 1] }
+  validates :author_id, uniqueness: { scope: [:votable_type, :votable_id], case_sensitive: false }
 
-  # validates :author_id, uniqueness: { scope: [:votable_type, :votable_id], case_sensitive: false }
+  def voting(state)
+    if twice?(state)
+      self.destroy!
+    else
+      self.state = state
+      self.save!
+    end
+  end
 
-  # def voting(state)
-  #   if twice?(state)
-  #     self.destroy!
-  #   else
-  #     self.state = state
-  #     self.save!
-  #   end
-  # end
-
-  # def twice?(state)
-  #   self.state == state
-  # end
+  def twice?(state)
+    self.state == state
+  end
 
 end
